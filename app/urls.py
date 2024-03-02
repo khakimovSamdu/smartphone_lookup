10e749e20ccfb81b929be2c1517a357a87f4c564
khakimovSamdu <allamurodxakimov@gmail.com>
2024-03-02 14:40:29 +0500

    failed lookups

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import get_add, get_all, get_id, brend_item_delete, brend_item_update, smartphone_orderby, smartphone_filter, smartphone_lookup
+from .views import contains, icontains, data_in, gt, gte, lt, lte, startswith, endswith, data_range
+from .views import get_year, get_month, get_day, get_week, get_week_day, get_quarter, bool_isnull, get_regex
+
 urlpatterns = [
     path('add/', get_add),
     path('all/', get_all),
@@ -9,6 +12,24 @@ urlpatterns = [
     path('orderby/', smartphone_orderby),
     path('filter/', smartphone_filter),
     path('lookup/', smartphone_lookup),
+    path('contains/', contains),
+    path('icontains/', icontains),
+    path('in/', data_in),
+    path('gt/<int:n>/', gt),
+    path('gte/<int:n>/', gte),
+    path('lt/<int:n>/', lt),
+    path('lte/<int:n>/', lte),
+    path('startswith/', startswith),
+    path('endswith/', endswith),
+    path('range/', data_range),
+    path('year/<int:year>/', get_year),
+    path('month/<int:month>/', get_month),
+    path('day/<int:day>/', get_day),
+    path('week/<int:week>/', get_week),
+    path('week_day/<int:week_day>/', get_week_day),
+    path('quarter/<int:n>/', get_quarter),
+    path('isnull/', bool_isnull),
+    path('regex/<s>/', get_regex),
     
-
+    
 ]
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 import json
 from .models import Smartphone
+from datetime import date
 from practikum.main import get_random_date
 # Create your views here.
 
@@ -92,6 +93,142 @@ def smartphone_filter(request: HttpRequest):
 def smartphone_lookup(request: HttpRequest):
     data = Smartphone.objects.filter(name__icontains="Apple")
     print(len(data))
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def contains(request:HttpRequest):
+    # Case-sensitive containment test. uzb Kata-kichik harflar sezgirligi testi
+    inner_qs = Smartphone.objects.filter(name__contains = "apple").values("name")
+    data = Smartphone.objects.filter(name__contains = "vivo")
+    print(len(data))
+    ruyxat = []
+    for item in inner_qs:
+            ruyxat.append(item)
+    return JsonResponse(ruyxat, safe=False)
+
+def icontains(request: HttpRequest):
+    #Case-insensitive containment test. uzb Katta - kichik harflarni sezmaydigan testi
+    data = Smartphone.objects.filter(color__icontains = 'gold')
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def data_in(request: HttpRequest):
+    inner_qs = Smartphone.objects.filter(name__contains='apple')
+    data = Smartphone.objects.filter(RAM__in = [2,4,6])
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def gt(request: HttpRequest, n: int):
+    data = Smartphone.objects.filter(memory__gt=n)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def gte(request: HttpRequest, n: int):
+    data = Smartphone.objects.filter(id__gte=n)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def lt(request: HttpRequest, n: int):
+    data = Smartphone.objects.filter(memory__lt=n)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def lte(request: HttpRequest, n: int):
+    data = Smartphone.objects.filter(id__lte=n, id__gte=1)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def startswith(request: HttpRequest):
+    data = Smartphone.objects.filter(id__startswith=2)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def endswith(request: HttpRequest):
+    data = Smartphone.objects.filter(id__endswith= 1)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def data_range(request: HttpRequest):
+    start_date = date(2016, 1, 1)
+    end_date = date(2024, 1, 1)
+    data = Smartphone.objects.filter(release_date__range=(start_date, end_date))
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def get_year(request: HttpRequest, year: int):
+    data = Smartphone.objects.filter(release_date__year__gte=year)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def get_month(request: HttpRequest, month: int):
+    data = Smartphone.objects.filter(release_date__month__lte=month)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+    
+def get_day(request: HttpRequest, day: int):
+    data = Smartphone.objects.filter(release_date__day=day)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def get_week(request: HttpRequest, week: int):
+    data = Smartphone.objects.filter(release_date__week__gte=1, release_date__week__lte=week)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def get_week_day(request: HttpRequest, week_day: int):
+    data = Smartphone.objects.filter(release_date__week_day = week_day)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def get_quarter(request: HttpRequest, n: int):
+    #Yil choragini ifodalovchi 1 dan 4 gacha bo'lgan butun qiymatni oladi.
+    data = Smartphone.objects.filter(release_date__quarter = n)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def bool_isnull(request: HttpRequest):
+    data = Smartphone.objects.filter(name__isnull=False)
+    ruyxat = []
+    for item in data:
+            ruyxat.append(item.to_dict())
+    return JsonResponse(ruyxat, safe=False)
+
+def get_regex(request: HttpRequest, s: str):
+    # Katta-kichik harflarga sezgir muntazam ifoda mosligi.
+    
+    data = Smartphone.objects.filter(color__regex = s)
     ruyxat = []
     for item in data:
             ruyxat.append(item.to_dict())
